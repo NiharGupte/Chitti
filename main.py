3f83a1a038d00563f13e08d03934731f256abc88
nihargupte <nihar71298@gmail.com>
2022-10-04 12:58:39 +0530

    Commit final changes

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ import argparse
 path = r"Data_chitti_final"
 
 if "Responses.csv" not in os.listdir(os.getcwd()):
-    print("Dataset doesn't exist. Creating the dataset ... ")
     dataframe(path)
-    print("Dataset created")
-else:
-    print("Dataset already exists")
 
 df = pd.read_csv("Responses.csv")
 text = list(df["text"].values)
 
-search("how to do two factor authentication", text)
+search("what is the full form of UGAC", text)
 
--- retrieval.py
+++ retrieval.py
@@ -32,7 +32,6 @@ def search(query, passages):
     question_embedding = bi_encoder.encode(query, convert_to_tensor=True)
     hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=top_k)
     hits = hits[0]
-    #print([text[hit['corpus_id']] for hit in hits])
     cross_inp = [[query, passages[hit['corpus_id']]] for hit in hits]
     cross_scores = cross_encoder.predict(cross_inp)
 
@@ -40,16 +39,6 @@ def search(query, passages):
     for idx in range(len(cross_scores)):
         hits[idx]['cross-score'] = cross_scores[idx]
 
-    # Output of top-20 hits from bi-encoder
-    print("\n-------------------------\n")
-    print("Top-20 Bi-Encoder Retrieval hits")
-    hits = sorted(hits, key=lambda x: x['score'], reverse=True)
-    for hit in hits[0:20]:
-        print("\t{:.3f}\t{}".format(hit['score'], passages[hit['corpus_id']].replace("\n", " ")))
-
-    # Output of top-5 hits from re-ranker
-    print("\n-------------------------\n")
-    print("Top-5 Cross-Encoder Re-ranker hits")
     hits = sorted(hits, key=lambda x: x['cross-score'], reverse=True)
     for hit in hits[0:5]:
-        print("\t{:.3f}\t{}".format(hit['cross-score'], passages[hit['corpus_id']].replace("\n", " ")))
+        print("{}".format(passages[hit['corpus_id']].replace("\n", " ")))
